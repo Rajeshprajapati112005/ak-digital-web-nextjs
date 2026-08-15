0e8333b353cb7539decbcd6c1dc54a4a69566137
Rajesh Prajapati <rajeshprajapati112005@gmail.com>
2026-08-15 20:16:25 +0530

    Fix testimonials JSX

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -41,7 +41,30 @@ export default function Testimonials() {
   return (
     <section id="testimonials" className="bg-[#f5f1ee] py-16   mt-6">
       <div className="container-px  mx-auto  text-center">
-        <p className="section-label justify-center about-label mb-3"><span class="about-label-icon"><svg xmlns="http://www.w3.org/2000/svg" width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-sparkles"><path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"></path><path d="M20 3v4"></path><path d="M22 5h-4"></path><path d="M4 17v2"></path><path d="M5 18H3"></path></svg></span>They trust us</p>
+        <p className="section-label justify-center about-label mb-3">
+  <span className="about-label-icon">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="11"
+      height="11"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-sparkles"
+    >
+      <path d="M9.937 15.5A2 2 0 0 0 8.5 14.063l-6.135-1.582a.5.5 0 0 1 0-.962L8.5 9.936A2 2 0 0 0 9.937 8.5l1.582-6.135a.5.5 0 0 1 .963 0L14.063 8.5A2 2 0 0 0 15.5 9.937l6.135 1.581a.5.5 0 0 1 0 .964L15.5 14.063a2 2 0 0 0-1.437 1.437l-1.582 6.135a.5.5 0 0 1-.963 0z"
+      />
+      <path d="M20 3v4" />
+      <path d="M22 5h-4" />
+      <path d="M4 17v2" />
+      <path d="M5 18H3" />
+    </svg>
+  </span>
+  They trust us
+</p>
          <h2 className="font-serif md:text-[52px] text-[40px] tracking-tight leading-none">
         Real feedback,{' '}
         <span className="text-red">real results.</span>
